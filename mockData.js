0abce67ef905e527f82c9f55a3fc180dbab7e184
ivan <ivanmoll07@gmail.com>
2023-09-09 11:30:00 +0200

    Add local mock data

---
+++ mockData.js
@@ -0,0 +1,56 @@
+export const movies = [
+  {
+    title: "Captain Marvel",
+    year: 2019,
+    director: "Anna Boden, Ryan Fleck",
+    duration: 123,
+    poster: "https://www.ecartelera.com/carteles/8900/8906/004_p.jpg",
+    rate: 6.3,
+    genres: ["Action", "Sci-fi"],
+  },
+  {
+    title: "Inception",
+    year: 2010,
+    director: "Christopher Nolan",
+    duration: 148,
+    poster: "https://www.ecartelera.com/carteles/10100/10108/001_p.jpg",
+    rate: 8.8,
+    genres: ["Action", "Sci-fi", "Thriller"],
+  },
+  {
+    title: "The Shawshank Redemption",
+    year: 1994,
+    director: "Frank Darabont",
+    duration: 142,
+    poster: "https://www.ecartelera.com/carteles/800/819/001_p.jpg",
+    rate: 9.3,
+    genres: ["Drama", "Crime"],
+  },
+  {
+    title: "Avatar",
+    year: 2009,
+    director: "James Cameron",
+    duration: 162,
+    poster: "https://www.ecartelera.com/carteles/10100/10131/001_p.jpg",
+    rate: 7.8,
+    genres: ["Action", "Adventure", "Sci-fi"],
+  },
+  {
+    title: "The Matrix",
+    year: 1999,
+    director: "Lana Wachowski, Lilly Wachowski",
+    duration: 136,
+    poster: "https://www.ecartelera.com/carteles/800/803/001_p.jpg",
+    rate: 8.7,
+    genres: ["Action", "Sci-fi"],
+  },
+];
+
+export const genres = [
+  { name: "Action" },
+  { name: "Sci-fi" },
+  { name: "Thriller" },
+  { name: "Drama" },
+  { name: "Crime" },
+  { name: "Adventure" },
+];
